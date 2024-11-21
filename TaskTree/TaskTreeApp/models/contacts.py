6996b51cb20730a2e3e218156f69857293f476c0
1 <v_derkach@inbox.ru>
2024-11-21 15:22:14 +0500

    2111

--- TaskTree/TaskTreeApp/models/contacts.py
+++ TaskTree/TaskTreeApp/models/contacts.py
@@ -1,16 +1,17 @@
-"""
-    Модуль models.contacts.py
 
-    Модель таблицы Контакты (Contacts).
-    поля Фамилия - last_name, Имя - first_name, Отчество - second_name.
-    ключевое поле id - 36 значная строка формата uuid
-"""
 from ..backend.db import engine, Base
 from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 class Contacts(Base):
+    """
+        Модуль models.contacts.py
+
+        Модель таблицы Контакты (Contacts).
+        поля Фамилия - last_name, Имя - first_name, Отчество - second_name.
+        ключевое поле id - 36 значная строка формата uuid
+    """
     __tablename__ = 'contacts'
     __table_args__ = {'keep_existing': True}
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
--- TaskTree/TaskTreeApp/models/tasks.py
+++ TaskTree/TaskTreeApp/models/tasks.py
@@ -1,17 +1,18 @@
-"""
-    Модуль models.tasks.py
-    Задает модель таблицы Tasks
-    поля Текст задачи (title),
-    Дата начала выполнения задачи Start,
-    Дата завершения задачи End.
-    ключевое поле id - 36 значная строка формата uuid
-"""
+
 from ..backend.db import Base, engine
 from sqlalchemy import Column, Text, Date
 from sqlalchemy.dialects.postgresql import UUID
 # from sqlalchemy import *
 import uuid
 class Tasks(Base):
+    """
+        Модуль models.tasks.py
+        Задает модель таблицы Tasks
+        поля Текст задачи (title),
+        Дата начала выполнения задачи Start,
+        Дата завершения задачи End.
+        ключевое поле id - 36 значная строка формата uuid
+    """
     __tablename__ = 'tasks'
     __table_args__ = {'keep_existing': True}
     # _query_cls = Query
